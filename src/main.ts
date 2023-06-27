c82f02bd51d25c3c7bc5efcc2fda2a91a31a6800
Mohammad Moiz Ali <muhammad.moiz@takafulbazaar.pk>
2023-06-27 15:58:37 +0500

    add ValidationPipe in main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { MessagesModule } from './messages/messages.module';
+import { ValidationPipe } from '@nestjs/common';
 // import { AppModule } from './app.module';
 
 async function bootstrap() {
   // const app = await NestFactory.create(AppModule);
   const app = await NestFactory.create(MessagesModule);
+  app.useGlobalPipes(
+    new ValidationPipe()
+  )
   await app.listen(3000);
 }
 bootstrap();
