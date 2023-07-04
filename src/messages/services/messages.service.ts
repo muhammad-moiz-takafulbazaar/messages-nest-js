8780e9fff89d288a04c30912e1cdacc3316760e8
Mohammad Moiz Ali <muhammad.moiz@takafulbazaar.pk>
2023-07-04 12:16:30 +0500

    handle constructor in service file

--- src/messages/services/messages.service.ts
+++ src/messages/services/messages.service.ts
@@ -1,3 +1,11 @@
 import { MessagesRepository } from "../repositories/messages.repository";
 
-export class MessagesService {}
+export class MessagesService {
+    messagesRepo: MessagesRepository;
+    
+    constructor() {
+        // Service is creating it's own dependencies
+        // Don't do this on real app
+        this.messagesRepo = new MessagesRepository()
+    }
+}
