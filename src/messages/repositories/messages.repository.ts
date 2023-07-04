3aed4863f13d00321a979632266bd2ddafbe06de
Mohammad Moiz Ali <muhammad.moiz@takafulbazaar.pk>
2023-07-04 12:01:26 +0500

    functional findAll function

--- src/messages/repositories/messages.repository.ts
+++ src/messages/repositories/messages.repository.ts
@@ -9,7 +9,10 @@ class MessagesRepository {
     }
 
     async findAll() {
+        const contents = await readFile('messages.json', 'utf8');
+        const messages = JSON.parse(contents);
 
+        return messages;
     }
 
     async create(message: string) {
