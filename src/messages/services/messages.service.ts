b228956042359899fb5c8572a8d8656aafc65839
Mohammad Moiz Ali <muhammad.moiz@takafulbazaar.pk>
2023-07-06 16:53:10 +0500

    refactor service file

--- src/messages/services/messages.service.ts
+++ src/messages/services/messages.service.ts
@@ -1,13 +1,7 @@
 import { MessagesRepository } from "../repositories/messages.repository";
 
 export class MessagesService {
-    messagesRepo: MessagesRepository;
-    
-    constructor() {
-        // Service is creating it's own dependencies
-        // Don't do this on real app
-        this.messagesRepo = new MessagesRepository()
-    }
+    constructor(public messagesRepo: MessagesRepository) {}
 
     async findOne(id: string) {
         return this.messagesRepo.findOne(id);
